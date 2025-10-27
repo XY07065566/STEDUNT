81fa5ead1e2cd1318db58690ec419fc966bc3e2d
XY07065566 <s1140339@dmhs.kh.edu.tw>
2025-10-27 16:20:51 +0800

    Add files via upload

---
+++ hw2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+	int age = 15;
+	double height = 172;
+	std::string name = "ricky";
+	bool  is_student = false;
+	std::cout <<  "name: " << name << std::endl;
+	std::cout <<  "h:+i+: " << height << std::endl;
+	std::cout <<  "e94i=!: " << age << std::endl;
+	std::cout <<  "f/e&g:e-8g: " << is_student << std::endl;
+
+
+	return 0;
+}
